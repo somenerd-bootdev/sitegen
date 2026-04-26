5e308d0f9943b7f9d22a18b7e8e544f200f577f2
somenerd_bootdev <somenerdbootdev@gmail.com>
2026-04-26 18:42:15 -0400

    Now with flexible basepaths

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from textnode import TextNode, TextType
 from pagebuilder import copy_dir, generate_page, generate_pages_recursive
-import os, shutil
+import os, shutil, sys
 
 
 def main():
-    destination = "public"
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    destination = "docs"
     if os.path.exists(destination):
         shutil.rmtree(destination)
     copy_dir("static", destination)
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", destination, basepath)
 
 main()
--- src/pagebuilder.py
+++ src/pagebuilder.py
@@ -22,7 +22,7 @@ def extract_title(markdown:str):
     lines = markdown.split("\n")
     return lines[0].lstrip("# ")
     
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     incoming_markdown = open(from_path).read()
     template = open(template_path).read()
@@ -30,11 +30,13 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(incoming_markdown)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace("href=\"/", f"href=\"{basepath}")
+    template = template.replace("src=\"/", f"src=\"{basepath}")
     if not os.path.exists(os.path.dirname(dest_path)):
         os.makedirs(os.path.dirname(dest_path))
     open(dest_path, "w").write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     contents = os.listdir(dir_path_content)
     for content in contents:
         content_dir_path = os.path.join(dir_path_content, content)
@@ -42,7 +44,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         if os.path.isfile(content_dir_path):
             if content.endswith(".md"):
                 md_dest_dir_path = Path(next_dest_dir_path).with_suffix(".html")
-                generate_page(content_dir_path, template_path, md_dest_dir_path)
+                generate_page(content_dir_path, template_path, md_dest_dir_path, basepath)
         else:
-            generate_pages_recursive(content_dir_path, template_path, next_dest_dir_path)
+            generate_pages_recursive(content_dir_path, template_path, next_dest_dir_path, basepath)
         
